5c39141a91dafa1c2698ffd6c68188d6f25af9a1
mariajesus <m.jesus_ts15@hotmail.com>
2019-11-12 19:28:24 +0100

    version 1 final

--- js/juego2.js
+++ js/juego2.js
@@ -12,10 +12,7 @@ function aleatorioNum(){
             partida[i][j]= Math.floor(Math.random() * (13 - 1)) + 1;//creo aleatorios
         }//end for j para carta     
     }//end for i para el jugador
-   
-    document.getElementById("num1").innerHTML="carta primera: " + partida[0][0] + " " + "carta segunda: " + partida[0][1];
-    document.getElementById("num2").innerHTML="carta primera: " + partida[1][0] +  " " + "carta segunda: " +partida[1][1];
-   
+    
     for(i=0;i<partida.length;i++){ //for para sacar el mayor de la carta
         partida[i].sort(function(a, b) { //function para ordenar num en array
             return a - b;
@@ -23,7 +20,6 @@ function aleatorioNum(){
         var jug1=partida[0][1];
         var jug2=partida[1][1];
     }
-    document.getElementById("demo").innerHTML="carta más alta del jugador 1:"+ " "+ jug1 +"carta más alta del jugador 2:"+ " "+ jug2;
 
     //if para comparar cartas mas alta de cada jugador
     if(jug1>jug2){
@@ -31,7 +27,7 @@ function aleatorioNum(){
     }else if(jug1<jug2){
         document.getElementById("resultado").innerHTML="el jugador 2";
     }else if(jug1===jug2){
-        document.getElementById("resultado").innerHTML="no hay ganador, nuevos iguales";
+        document.getElementById("resultado").innerHTML="no hay ganador, numeros iguales";
 
     }
     
