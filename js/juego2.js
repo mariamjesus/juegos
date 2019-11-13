5ced4c9d9b01a1ef9216b75fec92ef676548b72f
mariajesus <m.jesus_ts15@hotmail.com>
2019-11-13 17:13:41 +0100

    cambio final

--- js/juego2.js
+++ js/juego2.js
@@ -5,6 +5,7 @@ function aleatorioNum(){
 
     var partida=new Array(nJugadores);
     var i,j;
+    var jug1CartMax, jug2CartMax;
  //FOR BIDIMENSIONAL PARA RELLENAR JUGADORES CON SUS CARTAS
     for(i=0;i<partida.length;i++){
         partida[i]=new Array(2);//creo array dibimensional para las cartas, 2
@@ -17,16 +18,16 @@ function aleatorioNum(){
         partida[i].sort(function(a, b) { //function para ordenar num en array
             return a - b;
         }); //posicion donde esta guardada el mayor
-        var jug1=partida[0][1];
-        var jug2=partida[1][1];
-    }
-
+        
+    }//end for
+    jug1CartMax=partida[0][1];
+    jug2CartMax=partida[1][1];
     //if para comparar cartas mas alta de cada jugador
-    if(jug1>jug2){
+    if(jug1CartMax>jug2CartMax){
         document.getElementById("resultado").innerHTML="el jugador 1";
-    }else if(jug1<jug2){
+    }else if(jug1CartMax<jug2CartMax){
         document.getElementById("resultado").innerHTML="el jugador 2";
-    }else if(jug1===jug2){
+    }else if(jug1CartMax===jug2CartMax){
         document.getElementById("resultado").innerHTML="no hay ganador, numeros iguales";
 
     }
